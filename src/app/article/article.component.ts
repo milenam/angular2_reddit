eb81e58c200ba225ccf4535e2ba516e12f22fadf
Milena Maltsev <milenamaltsev@Milenas-MacBook-Pro.local>
2017-01-19 21:00:34 -0500

    reddit is done

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Article} from './article.model';
 @Component({
   selector: 'app-article',
@@ -11,14 +11,7 @@ import {Article} from './article.model';
 
 export class ArticleComponent implements OnInit {
 
-  	article: Article;
-    
-	constructor() {
-		this.article = new Article(
- 			'Angular 2',
- 			'http://angular.io',
- 			10);
-	}
+  	@Input() article: Article;
 
 	voteUp() {
 		this.article.voteUp();
